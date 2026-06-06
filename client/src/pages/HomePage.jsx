1b8665e6156fad719445b756613d1cd638ae9897
Harsh Patel <harshpatel15505@gmail.com>
2026-06-06 18:57:25 +0530

    refactor: replace ChatComponent with ChatContainer and add utility for message time formatting

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
 import React , {useState }from 'react'
 import Sidebar from '../components/Sidebar'
 import RightSidebar from '../components/RightSidebar'
-import ChatComponent from '../components/ChatComponent'
+import  ChatContainer from '../components/ChatContainer'
 const HomePage = () => {
     const [selectedUser, setSelectedUser] = useState(false)
   return (
@@ -9,7 +9,7 @@ const HomePage = () => {
        <div className={`backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-[100%]
         grid grid-cols-1 relative ${selectedUser ? 'md:grid-cols-[1fr_1,5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]' : 'md:grid-cols-2'}`}>
           <Sidebar selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
-          <ChatComponent selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
+          <ChatContainer selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
           <RightSidebar selectedUser={selectedUser} setSelectedUser={setSelectedUser}/>
        </div>
     </div>
