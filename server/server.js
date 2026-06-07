52b579dde49d72df4c7e50180e219130118d9704
Harsh Patel <harshpatel15505@gmail.com>
2026-06-07 23:19:47 +0530

    add MongoDB connection to server initialization

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import http from "http";
+import { connectDB } from "./lib/db.js";
 
 // create express app and http server
 const app = express();
@@ -14,6 +15,9 @@ app.use(express.json(
 app.use(cors());
 
 app.use("/api/status",(req,res)=>res.send("Server is live"));
+
+// Connect to MongoDB
+await connectDB()
 
 const PORT = process.env.PORT || 5000;
 
