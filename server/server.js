6aed0221f8117cd51f8ee4d025ce88e4a2be71d0
Harsh Patel <harshpatel15505@gmail.com>
2026-06-09 07:51:52 +0530

    refactor(server): reorganize imports and enhance middleware setup for clarity

--- server/server.js
+++ server/server.js
@@ -3,26 +3,30 @@ import "dotenv/config";
 import cors from "cors";
 import http from "http";
 import { connectDB } from "./lib/db.js";
+import userRouter from "./routes/user.route.js";
+import messageRouter from "./routes/message.route.js";
+import { attachSocketServer } from "./lib/socket.js";
 
 // create express app and http server
 const app = express();
 const server = http.createServer(app);
 
-//Middleware setup
-app.use(express.json(
-    {limit:"4mb"}
-));
+// Initialize socket.io server and handlers
+attachSocketServer(server);
+
+// Middleware setup
+app.use(express.json({ limit: "4mb" }));
 app.use(cors());
 
-app.use("/api/status",(req,res)=>res.send("Server is live"));
+// Routes Setup
+app.use("/api/status", (req, res) => res.send("Server is live"));
+app.use("/api/auth", userRouter);
+app.use("/api/messages", messageRouter);
 
 // Connect to MongoDB
-await connectDB()
+await connectDB();
 
 const PORT = process.env.PORT || 5000;
-
-server.listen(PORT,()=>
-console.log("Server is listening on PORT:"+PORT)
-)
+server.listen(PORT, () => console.log("Server is listening on PORT:" + PORT));
 
 
